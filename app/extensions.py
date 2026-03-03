5d200b4aa24414163369094a0a12b34697384a5f
eallianaciber <ealliana.ciber@gmail.com>
2026-03-03 15:21:46 -0300

    Delete app/extensions.py

--- app/extensions.py
+++
@@ -1,8 +0,0 @@
-# app/extensions.py
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
